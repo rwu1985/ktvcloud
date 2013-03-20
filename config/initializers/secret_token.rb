847e7438b94a3b8685e132985324ab2ce0c016a5
RW <rwu1985@gmail.com>
2013-03-19 22:44:15 -0700

    new project for ktv

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ktvcloud::Application.config.secret_token = 'e781ec5d6f949248fc5c982fa917e374e7508385001b2936013413ca7c84f045fd185f53ee3efef7e9c09c84a6e5ea39392fd13888b86d007142f89590599220'
